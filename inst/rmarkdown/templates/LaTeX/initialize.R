924d895f813aad78f1a395740269252ce5dd2308
Sam Rogers <7007561+rogerssam@users.noreply.github.com>
2019-11-19 13:04:13 +1030

    Updated to use knitr

--- inst/rmarkdown/templates/LaTeX/initialize.R
+++ inst/rmarkdown/templates/LaTeX/initialize.R
@@ -1,4 +1,4 @@
 options(formatR.arrow=FALSE, xtable.comment=FALSE)
-opts_chunk$set(fig.path='figure/Rplots-',fig.align='center',fig.show='hold',comment=NA,background='white',highlight=FALSE, tidy=TRUE, tidy.opts=list(width.cutoff=60),size= "small",prompt=TRUE)
+knitr::opts_chunk$set(fig.path='figure/Rplots-',fig.align='center',fig.show='hold',comment=NA,background='white',highlight=FALSE, tidy=TRUE, tidy.opts=list(width.cutoff=60),size= "small",prompt=TRUE)
 
 
