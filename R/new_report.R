1623c6c0ee069e025b7bec413162a91ac44553b0
Sam Rogers <7007561+rogerssam@users.noreply.github.com>
2022-08-16 15:12:48 +0930

    Updating function

--- R/new_report.R
+++ R/new_report.R
@@ -1,10 +1,13 @@
 #' Create a new Biometry Hub Report using a template
 #'
 #' @param filename The filename of the report
-#' @param type The type of the report. Valid types are `biometry_hub_report`, `sagi_report`, `html_presentation`.
+#' @param type The type of the report. Valid types are `biometry_hub_report`, `sagi_report`, `html_presentation`, `short_report` and `knitr_report`.
 #'
 #' @importFrom rmarkdown draft
 #' @importFrom utils file.edit
+#' @importFrom bookdown pdf_document2
+#' @importFrom knitr opts_chunk
+#' @importFrom xaringan inf_mr
 #'
 #' @return Returns the path to the created file invisibly.
 #' @export
@@ -14,18 +17,30 @@
 #' new_report(filename = "test_report", type = "biometry_hub_report")
 #' }
 new_report <- function(filename, type) {
-  stopifnot(tolower(type) %in% c("biometry_hub_report", "sagi_report", "html_presentation"))
+  type <- match.arg(tolower(type),
+                    choices = c("biometry_hub_report", "sagi_report", "html_presentation", "short_report", "knitr_report"))
 
-  if(tolower(type) %in% c("biometry_hub_report", "bhr", "bh")) {
+  if(type == "biometry_hub_report") {
     bh_report(filename)
+    ext <- "Rmd"
   }
-  else if(tolower(type) %in% c("sagi_report", "sagi", "grdc")) {
+  else if(type == "sagi_report") {
     sagi_report(filename)
+    ext <- "Rmd"
   }
-  else if(tolower(type)  %in% c("html_presentation", "presentation", "pres", "slides")) {
+  else if(type == "html_presentation") {
     html_presentation(filename)
+    ext <- "html"
   }
-  invisible(file.path(filename, paste0(filename, ".Rmd")))
+  else if(type == "short_report") {
+    short_report(filename)
+    ext <- "Rmd"
+  }
+  else if(type == "knitr_report") {
+    knitr_report(filename)
+    ext <- "Rnw"
+  }
+  invisible(file.path(filename, paste(filename, ext, sep = ".")))
 }
 
 #' @rdname new_report
@@ -62,22 +77,9 @@ html_presentation <- function(filename) {
 }
 
 #' @rdname new_report
-knitr_report <- function(filename) {
-  # create folder
-  if(!dir.exists(filename)) {
-    dir.create(trimws(filename))
-  }
-
-  # copy template and assets into folder
-  # file.copy(
-    file.path(find.package("SAGITemplates"),
-                      "inst", "rmarkdown", "templates")
-
-  # rename template
-  # Open for editing
-
+short_report <- function(filename) {
   rmarkdown::draft(file = filename,
-                   template = "HTML_presentation",
+                   template = "one_page_report",
                    package = "SAGITemplates",
                    create_dir = TRUE, edit = FALSE)
   file <- file.path(filename, paste0(filename, ".Rmd"))
@@ -86,12 +88,25 @@ knitr_report <- function(filename) {
 }
 
 #' @rdname new_report
-html_presentation <- function(filename) {
-  rmarkdown::draft(file = filename,
-                   template = "HTML_presentation",
-                   package = "SAGITemplates",
-                   create_dir = TRUE, edit = FALSE)
-  file <- file.path(filename, paste0(filename, ".Rmd"))
+knitr_report <- function(filename) {
+  # create folder
+  if(!dir.exists(filename)) {
+    dir.create(trimws(filename))
+  }
+
+  files <- list.files(file.path(find.package("SAGITemplates"),
+                     "inst", "rmarkdown", "templates", "Knitr"), full.names = TRUE)
+  # copy template and assets into folder
+  file.copy(from = files, to = filename, recursive = TRUE)
+
+  # rename template
+  file.rename(from = file.path(filename, "report.Rnw"),
+              to = file.path(filename, paste0(filename, ".Rnw")))
+
+  # Open for editing
+  file <- file.path(filename, paste0(filename, ".Rnw"))
   file.edit(file)
   invisible(file)
 }
+
+
